6978efdfbdf4f4cd732d4f17109d3548be2682d7
Browny <56095902+codingbrowny@users.noreply.github.com>
2023-01-05 20:09:23 +0000

    Delete src/@core directory

--- src/@core/components/navbar/nav-utils.ts
+++
@@ -1,6 +0,0 @@
-export const searchClassName =
-  "md:block py-3 px-5 pr-10 w-full text-sm text-gray-900 bg-gray-50 rounded-sm border outline-none border-gray-300 dark:focus:ring-blue-500 dark:focus:border-blue-500";
-
-//   Search Button
-export const searchButtonClass =
-    "text-white right-1 bottom-1 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm transition-all duration-100";
